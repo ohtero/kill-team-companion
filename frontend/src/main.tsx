eb02cf35e30f117e8d6cfdb06793612d63cc5b89
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-04-23 15:43:25 +0300

    fix: fix import paths

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { GlobalStyles } from './assets/global_styling/global-styles.ts';
+import { GlobalStyles } from './styling/globalStyles.ts';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './assets/global_styling/app-theme.ts';
+import { theme } from './styling/appTheme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
