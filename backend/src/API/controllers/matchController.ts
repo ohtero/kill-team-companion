c100c73a23f8f191934eab82538af0ba3e2fdb1a
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-04-23 15:28:30 +0300

    feat: add API routes and controllers for match data fetching and player name creation

--- backend/src/API/controllers/matchController.ts
+++ backend/src/API/controllers/matchController.ts
@@ -1,10 +1,7 @@
-import express from 'express';
+import { Request, Response } from 'express';
 import { nanoid } from 'nanoid';
 
-async function createMatch(
-  req: express.Request,
-  res: express.Response
-): Promise<void> {
+async function createMatch(req: Request, res: Response): Promise<void> {
   const client = req.dbClient;
   const matchName: string = req.body;
   const matchId: string = nanoid(10);
@@ -17,10 +14,46 @@ async function createMatch(
     res.status(200).json(matchId);
   } catch (err) {
     console.log(err);
-    res.status(500).json('Database action failed');
+    res.status(500).json('Server failed to create match.');
   } finally {
     client?.release();
   }
 }
 
-export { createMatch };
+async function getMatchData(req: Request, res: Response) {
+  const client = req.dbClient;
+  const matchId = req.params.matchId;
+  const query = {
+    text: 'SELECT * FROM matches WHERE (match_id) = ($1)',
+    values: [matchId]
+  };
+  try {
+    const data = await client?.query(query);
+    res.status(200).json(data?.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Server failed to fetch match data.');
+  }
+}
+
+async function addNewPlayer(req: Request, res: Response) {
+  const client = req.dbClient;
+  const matchId = req.params.matchId;
+  const { playerName, playerSlot } = req.body;
+  const player = `player${playerSlot + 1}_name`;
+  const query = {
+    text: `UPDATE matches SET ${player} = $1 WHERE match_id = $2 RETURNING ${player}`,
+
+    values: [playerName, matchId]
+  };
+  try {
+    const data = await client?.query(query);
+    const name = Object.values(data?.rows[0])[0];
+    res.status(200).json(name);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json('Server failed to add player.');
+  }
+}
+
+export { createMatch, getMatchData, addNewPlayer };
--- backend/src/API/routers/matchRouter.ts
+++ backend/src/API/routers/matchRouter.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { createMatch } from '../controllers/matchController';
+import {
+  createMatch,
+  getMatchData,
+  addNewPlayer
+} from '../controllers/matchController';
 
 const matchRouter = Router();
 
 matchRouter.post('/new-match', createMatch);
+matchRouter.get('/match-data/:matchId', getMatchData);
+matchRouter.post('/new-player/:matchId', addNewPlayer);
 
 export { matchRouter };
