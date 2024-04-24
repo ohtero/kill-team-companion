5838d545e7198a7d9025daad6817775c94ce0e2c
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-04-24 16:18:35 +0300

    feat: add database types to match types in backend and frontend

---
+++ frontend/src/types/databaseTypes.d.ts
@@ -0,0 +1,27 @@
+export interface MatchData {
+  match_id: string;
+  match_name: string;
+  mission_id: number;
+  active: boolean;
+  date: string; // Check if Date type is needed later
+  player1_id: string;
+  player2_id: string;
+  player3_id: string;
+  player4_id: string;
+  player1_name: string;
+  player2_name: string;
+  player3_name: string;
+  player4_name: string;
+  player1_cp: number;
+  player2_cp: number;
+  player3_cp: number;
+  player4_cp: number;
+  player1_vp: number;
+  player2_vp: number;
+  player3_vp: number;
+  player4_vp: number;
+  turning_point: number;
+  winner_id: string;
+  winner_name: string;
+  draw: boolean;
+}
