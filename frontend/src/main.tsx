8374fc4d1d411791ccaac96bf54a349091030da5
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-04-24 16:19:01 +0300

    fix: change wrong import path

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './styling/index.css';
 import { GlobalStyles } from './styling/globalStyles.ts';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styling/appTheme.ts';
