738c813b76c8cd793e14f8394e07c4d83975d6c0
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 12:40:15 +0300

    feat: add types for match components

--- frontend/src/pages/matchRoom/types.d.ts
+++ frontend/src/pages/matchRoom/types.d.ts
@@ -1,8 +1,12 @@
-export interface MatchEntryFormInput {
+import { Socket } from 'socket.io-client';
+import { NormalizedMatchData } from '../../types/databaseTypes';
+import { SubmitHandler } from 'react-hook-form';
+
+interface MatchEntryFormInput {
   displayName: string;
 }
 
-export interface MatchEntryProps {
+interface MatchEntryProps {
   matchId: string | null;
   matchData: MatchData;
   localNameData: string | null;
@@ -10,4 +14,65 @@ export interface MatchEntryProps {
   matchIsFull: boolean;
   updateMatchIsFull: (arg: boolean) => void;
   updatePlayerInMatch: (arg: boolean) => void;
+  socket?: Socket;
 }
+
+interface NameInputFormTypes {
+  socket?: Socket;
+  matchId: string | null;
+  matchIsFull: boolean;
+  updatePlayerInMatch?: (arg: boolean) => void;
+  onTestSubmit?: SubmitHandler;
+}
+
+interface MatchFeatureProps {
+  increaseTurnCount?: () => void;
+}
+
+interface FeatureTabProps {
+  children: React.ReactNode;
+  tabName: TabName;
+  activeTab: TabName;
+  handleClick: (tabName: TabName) => void;
+}
+
+interface TabNavProps {
+  activeTab: TabName;
+  updateActiveTab: (tabName: TabName) => void;
+}
+
+export type PlayerPoint = {
+  points: number;
+  playerIndex: number;
+  pointType: 'cp' | 'vp';
+};
+
+interface TurnPlayerPoint extends MatchFeatureProps {
+  increaseTurnCount?: () => void;
+}
+
+interface TurnIndicatorProps {
+  turnValue: number;
+  currentTurn: number;
+}
+
+export type TabName = 'details' | 'counters' | 'mission';
+
+type MatchContext = {
+  matchData: NormalizedMatchData;
+  updateMatchData: (matchData: NormalizedMatchData) => void;
+  updateSocket: (socket: Socket) => void;
+  modifyTurnCount: (modType: 'add' | 'subtract') => void;
+  modifyPlayerPoints: (
+    playerIndex: number,
+    pointType: 'vp' | 'cp',
+    modType: 'add' | 'subtract'
+  ) => void;
+  updatePlayerPoints: (
+    playerIndex: number,
+    newPoints: number,
+    pointType: 'cp' | 'vp'
+  ) => void;
+  updateTurnCount: (modType: 'add' | 'subtract') => void;
+  updatePlayerName: (name: string) => void;
+};
