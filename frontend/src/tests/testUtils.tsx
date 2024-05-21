fa0a223f1a75903478b57fe9eab2dce5ee536182
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 13:31:08 +0300

    test: create wrapper to handle custom providers

---
+++ frontend/src/tests/testUtils.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../globalStyling/appTheme';
+import { MemoryRouter } from 'react-router-dom';
+
+const AllProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <MemoryRouter>{children}</MemoryRouter>
+    </ThemeProvider>
+  );
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => {
+  render(ui, { wrapper: AllProviders, ...options });
+};
+
+export * from '@testing-library/react';
+export { customRender as render };
