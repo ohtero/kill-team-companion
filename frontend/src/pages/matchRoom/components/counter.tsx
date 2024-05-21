eeaebc101a95900389160cff6e23a26b4b00c47f
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 12:41:14 +0300

    feat: create counter component to track player points

---
+++ frontend/src/pages/matchRoom/components/counter.tsx
@@ -0,0 +1,77 @@
+import styled from 'styled-components';
+import { PlayerPoint } from '../types';
+import { useMatchContext } from '../context/matchContext';
+
+export function Counter({ points, playerIndex, pointType }: PlayerPoint) {
+  const { modifyPlayerPoints } = useMatchContext();
+  return (
+    <CounterContainer>
+      <div className="wrapper">
+        <button
+          className="add-point"
+          onClick={() => modifyPlayerPoints(playerIndex, pointType, 'add')}
+        >
+          +
+        </button>
+        <div className="point-display">
+          <output role="player-points">{points}</output>
+        </div>
+        <button
+          name="subtractPoint"
+          className="sub-point"
+          onClick={() =>
+            points > 0 && modifyPlayerPoints(playerIndex, pointType, 'subtract')
+          }
+        >
+          -
+        </button>
+      </div>
+    </CounterContainer>
+  );
+}
+
+const CounterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  align-items: center;
+
+  button {
+    // border: 4px solid #eee;
+    border: 4px solid HSL(200, 10%, 70%);
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    min-width: 50px;
+    width: clamp(50px, 100%, 1000px);
+    margin: 10px 0;
+  }
+
+  .wrapper > * {
+    height: 75px;
+  }
+
+  .point-display {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #fff;
+    font-size: 2.5rem;
+    font-weight: 700;
+    border: 4px solid #eee;
+  }
+
+  .add-point {
+    border-radius: 10px 10px 0 0;
+    font-size: 2.5rem;
+    background: linear-gradient(to top, HSL(200, 10%, 80%) 0%, #fff 50%);
+  }
+  .sub-point {
+    border-radius: 0 0 10px 10px;
+    font-size: 2.5rem;
+    background: linear-gradient(to bottom, HSL(200, 10%, 80%) 0%, #fff 50%);
+    // border: 4px solid HSL(200, 10%, 80%);
+  }
+`;
