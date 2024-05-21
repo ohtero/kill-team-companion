e9a4e6a29963f06f42752702263400c61396e0c6
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 13:33:19 +0300

    test: create mock values for match data context provider

---
+++ frontend/src/tests/mocks/matchData.ts
@@ -0,0 +1,53 @@
+import { it, expect, describe, vi } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+export const mockMatchData = {
+  matchId: '',
+  matchName: '',
+  missionId: NaN,
+  active: false,
+  date: '',
+  turningPoint: 1,
+  players: {
+    player1: {
+      id: '',
+      name: '',
+      cp: 3,
+      vp: 0
+    },
+    player2: {
+      id: '',
+      name: '',
+      cp: 3,
+      vp: 0
+    },
+    player3: {
+      id: '',
+      name: '',
+      cp: 3,
+      vp: 0
+    },
+    player4: {
+      id: '',
+      name: '',
+      cp: 3,
+      vp: 0
+    }
+  },
+  winner: {
+    id: '',
+    name: ''
+  },
+  draw: false
+};
+
+export const mockMatchDataContextValue = {
+  matchData: mockMatchData,
+  updateMatchData: vi.fn(),
+  updateSocket: vi.fn(),
+  modifyPlayerPoints: vi.fn(),
+  modifyTurnCount: vi.fn(),
+  updatePlayerPoints: vi.fn(),
+  updateTurnCount: vi.fn(),
+  updatePlayerName: vi.fn()
+};
