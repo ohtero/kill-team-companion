d11e6ac450e5c90ac00b3160a662ac040144a2bd
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 12:55:59 +0300

    feat: create turn indicator child component for turn counter

---
+++ frontend/src/pages/matchRoom/components/turnIndicator.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { TurnIndicatorProps } from '../types';
+
+export function TurnIndicator({ currentTurn, turnValue }: TurnIndicatorProps) {
+  return (
+    <>
+      <Indicator $currentTurn={currentTurn} $ownTurn={turnValue}>
+        {turnValue}
+      </Indicator>
+    </>
+  );
+}
+
+const Indicator = styled.li<{ $ownTurn: number; $currentTurn: number }>`
+  display: flex;
+  place-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 60px;
+
+  border: ${(props) =>
+    props.$ownTurn === props.$currentTurn
+      ? `4px solid HSLA(${props.theme.colors.tertiary})`
+      : 'none'};
+`;
