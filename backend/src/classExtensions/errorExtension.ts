87def8f8ee28ab4fae8cbb9459161bb931ebbc99
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 12:02:30 +0300

    refactor: make custom error description optional

--- backend/src/classExtensions/errorExtension.ts
+++ backend/src/classExtensions/errorExtension.ts
@@ -1,7 +1,7 @@
 export class AppError extends Error {
   public readonly name: string;
 
-  constructor(name: string, description: string) {
+  constructor(name: string, description?: string) {
     super(description);
     Object.setPrototypeOf(this, new.target.prototype);
     this.name = name;
