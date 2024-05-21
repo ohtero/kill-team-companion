8bc425c36d3c953730a5af8aab2a2804fda60748
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 12:01:21 +0300

    feat: create new API routes

--- backend/src/API/routers/matchRouter.ts
+++ backend/src/API/routers/matchRouter.ts
@@ -3,6 +3,8 @@ import {
   createMatch,
   getMatchData,
   addNewPlayer
+  // modifyPlayerPoints,
+  // modifyTurnCount
 } from '../controllers/matchController';
 
 const matchRouter = Router();
@@ -10,5 +12,7 @@ const matchRouter = Router();
 matchRouter.post('/new-match', createMatch);
 matchRouter.get('/match-data/:matchId', getMatchData);
 matchRouter.post('/new-player/:matchId', addNewPlayer);
+// matchRouter.post('/:matchId/:playerIndex/:point/:type', modifyPlayerPoints);
+// matchRouter.post('/turn/:matchId/:type', modifyTurnCount);
 
 export { matchRouter };
