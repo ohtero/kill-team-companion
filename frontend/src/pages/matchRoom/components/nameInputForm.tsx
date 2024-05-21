0592f6395301c03157d16cbc876bd8a3d5642b42
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-05-21 12:54:01 +0300

    refactor: extract  name input form into its own component

---
+++ frontend/src/pages/matchRoom/components/nameInputForm.tsx
@@ -0,0 +1,104 @@
+import { NameInputFormTypes, MatchEntryFormInput } from '../types';
+import { Form } from '../../../components/UI/form';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { ErrorMsg } from '../../../components/UI/errorMsg';
+import { FormSection } from '../../../components/UI/formSection';
+
+export function NameInputForm({
+  socket,
+  matchId,
+  matchIsFull,
+  updatePlayerInMatch,
+  onTestSubmit
+}: NameInputFormTypes) {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors }
+  } = useForm<MatchEntryFormInput>({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    delayError: 500
+  });
+
+  const addPlayerDisplayName: SubmitHandler<MatchEntryFormInput> = async (
+    formData
+  ) => {
+    const body = {
+      playerName: formData.displayName
+    };
+
+    try {
+      const res = await fetch(
+        `http://localhost:3000/match/new-player/${matchId}`,
+        {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(body)
+        }
+      );
+      let resData = '';
+      if (res) {
+        const data = (await res.json()) as string;
+        resData = data;
+      }
+      if (res.status === 200) {
+        localStorage.setItem('playerName', resData);
+        dispatchEvent(
+          new StorageEvent('storage', { key: 'playerName', newValue: resData })
+        );
+        updatePlayerInMatch && updatePlayerInMatch(true);
+        socket?.emit('newPlayer', { playerName: resData, matchId: matchId });
+      } else if (res.status === 500) {
+        throw new Error(resData);
+      }
+    } catch (error) {
+      const err = error as Error;
+      console.log(error);
+      setError('root.serverError', {
+        type: 'server',
+        message: `${err.message}`
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onTestSubmit ?? addPlayerDisplayName)}>
+      <FormSection>
+        <label htmlFor="displayName">Name</label>
+        <input
+          {...register('displayName', {
+            required: { value: true, message: 'Name is required' },
+            maxLength: { value: 20, message: 'Name is too long' },
+            validate: (value) =>
+              value.trim().length > 0 || "Name can't be only empty spaces."
+          })}
+          type="text"
+          name="displayName"
+          id="displayName"
+          placeholder="Your display name"
+          disabled={matchIsFull}
+        />
+      </FormSection>
+      <ErrorMsg $centered>
+        {errors.root?.serverError && errors.root?.serverError.message}
+        {errors.displayName && errors.displayName.message}
+      </ErrorMsg>
+      <FormSection>
+        <button type="submit" name="confirmDisplayName" disabled={matchIsFull}>
+          Confirm Name
+        </button>
+        <button
+          type="button"
+          name="spectateMatch"
+          onClick={() => updatePlayerInMatch && updatePlayerInMatch(true)}
+        >
+          Spectator
+        </button>
+      </FormSection>
+    </Form>
+  );
+}
--- frontend/src/pages/matchRoom/features/matchEntry.tsx
+++ frontend/src/pages/matchRoom/features/matchEntry.tsx
@@ -1,147 +1,50 @@
 import { ContentSection } from '../../../components/UI/contentSection';
 import { ContentHeader } from '../../../components/UI/contentHeader';
-import { Form } from '../../../components/UI/form';
-import { FormSection } from '../../../components/UI/formSection';
 import { useNavigate } from 'react-router-dom';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { MatchEntryFormInput, MatchEntryProps } from '../types';
-import { useEffect } from 'react';
-import { ErrorMsg } from '../../../components/UI/errorMsg';
+import { MatchEntryProps } from '../types';
+import { NameInputForm } from '../components/nameInputForm';
 
 export function MatchEntry({
   matchId,
-  matchData,
-  playerInMatch,
   matchIsFull,
-  updateMatchIsFull,
-  updatePlayerInMatch
+  updatePlayerInMatch,
+  socket
 }: MatchEntryProps) {
   const navigate = useNavigate();
 
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors }
-  } = useForm<MatchEntryFormInput>({
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    delayError: 500
-  });
-
-  useEffect(() => {
-    if (matchData.player4_name) {
-      updateMatchIsFull(true);
-    }
-  }, [matchData]);
-
-  const addPlayerDisplayName: SubmitHandler<MatchEntryFormInput> = async (
-    formData
-  ) => {
-    const body = {
-      playerName: formData.displayName
-    };
-
-    try {
-      const res = await fetch(
-        `http://localhost:3000/match/new-player/${matchId}`,
-        {
-          method: 'POST',
-          headers: {
-            'content-type': 'application/json'
-          },
-          body: JSON.stringify(body)
-        }
-      );
-      let resData = '';
-      if (res) {
-        const data = (await res.json()) as string;
-        resData = data;
-      }
-      if (res.status === 200) {
-        localStorage.setItem('playerName', resData);
-        dispatchEvent(
-          new StorageEvent('storage', { key: 'playerName', newValue: resData })
-        );
-        updatePlayerInMatch(true);
-      } else if (res.status === 500) {
-        throw new Error(resData);
-      }
-    } catch (error) {
-      const err = error as Error;
-      console.log(error);
-      setError('root.serverError', {
-        type: 'server',
-        message: `${err.message}`
-      });
-    }
-  };
-
   return (
     <>
-      {playerInMatch ? (
-        <p>asd</p>
-      ) : (
-        <ContentSection>
-          <ContentHeader>MATCH LOBBY</ContentHeader>
-          <section className="instructions">
-            <h3></h3>
-            {!matchIsFull ? (
-              <p>
-                Select your display name or choose 'Spectator' if you wish to
-                only spectate the match.
-              </p>
-            ) : (
-              <p>
-                Match is full! <br />
-                <br />
-                Click 'Spectator' to observe the match.
-              </p>
-            )}
-          </section>
-          <Form onSubmit={handleSubmit(addPlayerDisplayName)}>
-            <FormSection>
-              <label htmlFor="displayName">Name</label>
-              <input
-                {...register('displayName', {
-                  required: { value: true, message: 'Name is required' },
-                  maxLength: { value: 50, message: 'Name is too long' },
-                  validate: (value) =>
-                    value.trim().length > 0 ||
-                    "Name can't be only empty spaces."
-                })}
-                type="text"
-                name="displayName"
-                id="displayName"
-                placeholder="Your display name"
-                disabled={matchIsFull}
-              />
-            </FormSection>
-            <ErrorMsg $centered>
-              {errors.root?.serverError && errors.root?.serverError.message}
-            </ErrorMsg>
-            <FormSection>
-              <button
-                type="submit"
-                name="confirmDisplayName"
-                disabled={matchIsFull}
-              >
-                Confirm Name
-              </button>
-              <button type="button" name="spectateMatch">
-                Spectator
-              </button>
-            </FormSection>
-          </Form>
-          <button
-            type="button"
-            name="returnToMatchmaker"
-            onClick={() => navigate('/matchmaker')}
-          >
-            Return
-          </button>
-        </ContentSection>
-      )}
+      <ContentSection>
+        <ContentHeader>MATCH LOBBY</ContentHeader>
+        <section className="instructions">
+          <h3></h3>
+          {!matchIsFull ? (
+            <p>
+              Select your display name or choose 'Spectator' if you wish to only
+              spectate the match.
+            </p>
+          ) : (
+            <p>
+              Match is full! <br />
+              <br />
+              Click 'Spectator' to observe the match.
+            </p>
+          )}
+        </section>
+        <NameInputForm
+          socket={socket}
+          matchId={matchId}
+          matchIsFull={matchIsFull}
+          updatePlayerInMatch={updatePlayerInMatch}
+        />
+        <button
+          type="button"
+          name="returnToMatchmaker"
+          onClick={() => navigate('/matchmaker')}
+        >
+          Return
+        </button>
+      </ContentSection>
     </>
   );
 }
