d7cca08cf097c2811f0d94dc9fe40bb85175f935
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-06-06 14:48:59 +0300

    chore: add comment

--- frontend/src/pages/matchRoom/features/matchMission.tsx
+++ frontend/src/pages/matchRoom/features/matchMission.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+// TODO: Implement displaying of mission details when mission selection on match creation is implemented.
 
 export function MissionDetails() {
   return <p>Nothing here yet!</p>;
