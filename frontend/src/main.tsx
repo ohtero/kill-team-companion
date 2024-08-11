a77608218eafaa8e45d7fe2e340a5e3ac3a38603
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-08-11 13:24:50 +0300

    refactor: remove file extensions from imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { GlobalStyles } from './globalStyling/globalStyles.ts';
+import App from './App';
+import { GlobalStyles } from './globalStyling/globalStyles';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './globalStyling/appTheme.ts';
-import { MatchDataProvider } from './pages/matchRoom/context/matchContext.tsx';
+import { theme } from './globalStyling/appTheme';
+import { MatchDataProvider } from './pages/matchRoom/context/matchContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
