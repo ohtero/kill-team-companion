9b8e3e90c0eb03603a74d7af0f8f53e2f27e0308
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-09-27 14:41:58 +0300

    type: break request body type into separate types for clarity

--- backend/src/types/express/express.d.ts
+++ backend/src/types/express/express.d.ts
@@ -3,6 +3,18 @@ import { PoolClient } from 'pg';
 declare module 'express' {
   interface Request {
     dbClient?: PoolClient;
-    reqBody?: { matchName: string; playerName: string };
+    body: Body;
   }
+}
+
+interface Body {
+  matchName: string;
+  playerName: string;
+  registrationData: RegistrationData;
+}
+
+interface RegistrationData {
+  username: string;
+  email: string;
+  password: string;
 }
