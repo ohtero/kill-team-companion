cd1ad1dd63ce940757b61f1b063ff1399f2fd6b8
ohtero <91533120+Tuhto@users.noreply.github.com>
2024-09-27 14:50:41 +0300

    feature: add functionality for point player point modification to not execute if criteria is not met

--- frontend/src/pages/matchRoom/components/pointModifierButton.tsx
+++ frontend/src/pages/matchRoom/components/pointModifierButton.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { useMatchContext } from '../context/matchContext';
 import { PointModifyButtonProps } from '../types';
+import { NormalizedMatchData } from '../../../types/databaseTypes';
 
 export function PointModifierButton({
   children,
@@ -9,12 +10,28 @@ export function PointModifierButton({
   operation,
   className
 }: PointModifyButtonProps) {
-  const { modifyPlayerPoints } = useMatchContext();
+  const { modifyPlayerPoints, matchData } = useMatchContext();
+
+  function isAllowed() {
+    const player =
+      `player${playerIndex + 1}` as keyof NormalizedMatchData['players'];
+    const point = matchData.players[player][pointType];
+    if ((operation === 'subtract' && point > 0) || operation === 'add') {
+      return true;
+    }
+  }
+
+  const name =
+    operation === 'add'
+      ? `add ${pointType} to player${playerIndex + 1}`
+      : `subtract ${pointType} from player${playerIndex + 1}`;
   return (
     <ModifyButton
       className={className}
-      aria-label={`${operation} ${pointType} to player${playerIndex + 1}`}
-      onClick={() => modifyPlayerPoints(playerIndex, pointType, operation)}
+      aria-label={name}
+      onClick={() =>
+        isAllowed() && modifyPlayerPoints(playerIndex, pointType, operation)
+      }
     >
       {children}
     </ModifyButton>
